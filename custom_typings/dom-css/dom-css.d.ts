6eef03f03a14c874fbd5e48a7c620ebdfb9f949b
devdoomari.ubuntu.tablet <devdoomari@gmail.com>
2017-01-15 15:33:52 +0900

    fix missing lodash dep

--- custom_typings/dom-css/dom-css.d.ts
+++ custom_typings/dom-css/dom-css.d.ts
@@ -1,6 +1,6 @@
 declare module 'dom-css' {
   import { CSSProperties } from 'react';
   // const style =
-  const domCssFunc: (HTMLElement, style: CSSProperties) => void;
+  const domCssFunc: (element: HTMLElement, style: CSSProperties) => void;
   export = domCssFunc;
 }
