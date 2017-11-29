f78a1e9b021955e095317c5902c00bfc60e8d7f0
devdoomari.circleci <devdoomari@gmail.com>
2017-11-29 05:09:09 +0000

    generated from:881754f8febcc19c889391a8cb7c10f549849c41 triggered by:devdoomari [ci skip]

---
+++ lib_es6/ReactStyleStreamedElem.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { Observable, Subscription } from 'rxjs';
+import { HTMLAllElementsType } from './HTMLAllElementType';
+export declare type StyleStreamType = Observable<React.CSSProperties>;
+export declare type PropsType = React.HTMLProps<HTMLAllElementsType> & {
+    tagName: string;
+    styleStream: StyleStreamType;
+    style?: null;
+};
+export default class ReactStyleStreamedElem extends React.Component<PropsType> {
+    targetElem: HTMLElement;
+    __style?: React.CSSProperties;
+    styleSubscription: Subscription;
+    constructor(props: PropsType);
+    componentDidMount(): void;
+    componentWillUnmount(): void;
+    render(): React.DOMElement<Readonly<{
+        children?: React.ReactNode;
+    }> & Readonly<PropsType> & {
+        tagName: any;
+        styleStream: any;
+        style: React.CSSProperties;
+    } & {
+        ref: (targetElem: HTMLElement) => void;
+    }, HTMLElement>;
+}
